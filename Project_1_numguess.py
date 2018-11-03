90aceb8e03aee03d4d916c212e1b3a4d8be3a32c
Mikaela <42855095+mikacodeland@users.noreply.github.com>
2018-11-03 10:48:06 -0700

    Update Project_1_numguess.py

---
+++ Project_1_numguess.py
@@ -0,0 +1,55 @@
+import random
+
+
+def start_game():
+    print("***********************************************")
+    print("Welcome to the number guessing game! Good luck!")
+    print("***********************************************")
+    print ("""GAME RULES:
+                1. Pick a number between 1 and 10 until you guess the correct number
+                2. If you get the number choose whether you want continue.
+                3. TRY AND GET THE LOWEST SCORE!""")
+    attempt = 0
+    right_number = random.randint(1, 10)
+
+    while True:
+        try:
+            guess = int(input("Please choose a number between 1 and 10:  "))
+            if guess <= 0 or guess > 10:
+                raise ValueError ("Sorry that number is not in between 1 and 10. Try again.")
+        except ValueError as err:
+            print ("{}".format(err))
+
+        if 11 > guess > right_number:
+            attempt += 1
+            print("Too high.  Go lower.")
+            continue
+
+        elif 0 < guess < right_number:
+            attempt += 1
+            print("Too low. Go higher.")
+            continue
+
+        elif guess == right_number:
+            attempt += 1
+            high_score = attempt
+            print("You got it! You finished in {} attempts.".format(attempt))
+            print("The game will end now, unless you would like to play again.")
+            print("Would you like to play again: Y/N")
+            again = input("> ")
+            if again.upper() == "Y":
+                if high_score > attempt:
+                    print ("The HIGH SCORE so far is {}".format(high_score))
+                    continue
+                else:
+                    print("The HIGH SCORE so far is {}".format(high_score))
+                    continue
+            elif again.upper() == "N":
+                break
+
+
+
+
+if __name__ == '__main__':
+    # Kick off the program by calling the start_game function.
+    start_game()
